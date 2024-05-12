87347bc017284f2e7da6ee3dd0ff2a2859dcf167
Benjamin Gwynn <git@benjamingwynn.com>
2024-05-12 21:25:23 +0100

    tweak deploy script

--- serverLocalDeploy.sh
+++ serverLocalDeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-rm -rf node_modules .esdev .data dist
+rm -rf .esdev .data dist
 scp -r piweb.local:~/app/.data .
-rsync -r --progress --exclude=.git . piweb.local:~/app
-pnpm i
-ssh piweb.local 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; cd ~/app; pnpm i'
+rsync -r --progress --exclude=.git --exclude=node_modules . piweb.local:~/app
+# ssh piweb.local 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; cd ~/app; pnpm i'
 # ssh piweb.local 'touch ~/app/server/main.node.ts'
